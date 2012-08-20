276e4bdd8a3565991a3e241c25fd37403e2498d9
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2012-08-20 01:32:17 -0400

    style changes

--- lm.rb
+++ lm.rb
@@ -6,7 +6,7 @@ class LanguageModel
 =begin
 What's the best model for a 3LM?
 
-threelm[wordTwoBack][wordBack]
+threelm[word_two_back][word_back]
 => {tokencount => 13, {1 => "Jeremy", 2 => "Nate", 3 => "Jeremy", 4 => "Megan"}}
 => {tokencount => 13, [[1, "Jeremy"], [5, "Megan"], [7, "Nate"]]
 
@@ -15,18 +15,18 @@ threelm[wordTwoBack][wordBack]
 =end
 
   def initialize(pathglob,lambdaFunc = lambda{|a| a } )
-    trigramcounts = {}
-    unigramcounts = {}
-    unigramcounts.default = 0
+    trigram_counts = {}
+    unigram_counts = {}
+    unigram_counts.default = 0
 
-    bigramcountsmember = {}
-    bigramcountsmember.default = 0
-    bigramcounts = {} #e.g. {San => {Francisco => 50, Jose => 20} }
-    bigramcounts.default = bigramcountsmember
+    bigram_counts_member = {}
+    bigram_counts_member.default = 0
+    bigram_counts = {} #e.g. {San => {Francisco => 50, Jose => 20} }
+    bigram_counts.default = bigram_counts_member
 
-    trigramcountsmember = {}
-    trigramcountsmember.default = bigramcountsmember
-    trigramcounts.default = trigramcountsmember
+    trigram_counts_member = {}
+    trigram_counts_member.default = bigram_counts_member
+    trigram_counts.default = trigram_counts_member
 
     @engtagger = EngTagger.new
     #@complementizer_list = ['AZ', 'TH', 'TS', ]
@@ -40,34 +40,34 @@ threelm[wordTwoBack][wordBack]
 
 
 
-    @wordcount = 0
+    @word_count = 0
     files = Dir.glob(File.join(*pathglob))
     files.each do |f|
       next if File.basename(f) == '.' or File.basename(f) == '..' or !lambdaFunc.call(File.basename(f))
       File.open(f).each do |line|
         #line = line.split(/[.?!]|$/) 
         [line].each do |sentence|
-          splitSentence = sentence.strip().split(" ")
-          wordTwoBack = ""
-          wordBack = ""
-          splitSentence.each do |word|
+          split_sentence = sentence.strip().split(" ")
+          word_two_back = ""
+          word_back = ""
+          split_sentence.each do |word|
             word.gsub("—", "")
             word = word.downcase.gsub(/[_\(\),]/, "") #—
-            @wordcount += 1
+            @word_count += 1
 
-            unigramcounts[word] += 1
-            bigramInnerDict = bigramcounts[wordBack].clone
-            bigramInnerDict[word] = bigramInnerDict[word] +1
-            bigramcounts[wordBack] = bigramInnerDict
-            #trigramcounts[wordTwoBack][wordBack][word] += 1
-            trigramOneStepInside = trigramcounts[wordTwoBack].clone
-            trigramTwoStepsInside = trigramOneStepInside[wordBack].clone
-            trigramTwoStepsInside[word] += 1
-            trigramOneStepInside[wordBack] = trigramTwoStepsInside
-            trigramcounts[wordTwoBack] = trigramOneStepInside
+            unigram_counts[word] += 1
+            bigram_inner_dict = bigram_counts[word_back].clone
+            bigram_inner_dict[word] = bigram_inner_dict[word] +1
+            bigram_counts[word_back] = bigram_inner_dict
+            #trigram_counts[word_two_back][word_back][word] += 1
+            trigram_one_step_inside = trigram_counts[word_two_back].clone
+            trigram_two_steps_inside = trigram_one_step_inside[word_back].clone
+            trigram_two_steps_inside[word] += 1
+            trigram_one_step_inside[word_back] = trigram_two_steps_inside
+            trigram_counts[word_two_back] = trigram_one_step_inside
             
-            wordTwoBack = wordBack
-            wordBack = word
+            word_two_back = word_back
+            word_back = word
           end
         end
       end
@@ -79,40 +79,40 @@ threelm[wordTwoBack][wordBack]
     @trigramlm.default = @bigramlm
     @unigramlm = []
 
-    trigramcounts.each do |wordTwoBack, trigramCountsOneStepInside|
-      trigramLMOneStepInside = {}
+    trigram_counts.each do |word_two_back, trigram_counts_one_step_inside|
+      trigram_LM_one_step_inside = {}
       outerCount = 0
-      trigramCountsOneStepInside.each do |wordBack, trigramCountsTwoStepsInside|
+      trigram_counts_one_step_inside.each do |word_back, trigram_counts_two_steps_inside|
         outerCount += 1
         count = 0
-        trigramLMTwoStepsInside = []
-        trigramCountsTwoStepsInside.each do |word, myCount|
+        trigram_LM_two_steps_inside = []
+        trigram_counts_two_steps_inside.each do |word, myCount|
           count += myCount
-          trigramLMTwoStepsInside << [count, word]
+          trigram_LM_two_steps_inside << [count, word]
         end
-        trigramLMOneStepInside[wordBack] = {}
-        trigramLMOneStepInside[wordBack].default = {:tokencount => 0, :data => []}
-        trigramLMOneStepInside[wordBack][:tokencount] = count
-        trigramLMOneStepInside[wordBack][:data] = trigramLMTwoStepsInside
+        trigram_LM_one_step_inside[word_back] = {}
+        trigram_LM_one_step_inside[word_back].default = {:tokencount => 0, :data => []}
+        trigram_LM_one_step_inside[word_back][:tokencount] = count
+        trigram_LM_one_step_inside[word_back][:data] = trigram_LM_two_steps_inside
       end
-      @trigramlm[wordTwoBack] = trigramLMOneStepInside
+      @trigramlm[word_two_back] = trigram_LM_one_step_inside
     end
 
-    bigramcounts.each do |wordBack, innerHash|
+    bigram_counts.each do |word_back, inner_hash|
       count = 0
       innerLM = []
-      innerHash.each do |word, myCount|
+      inner_hash.each do |word, myCount|
         count += myCount
         innerLM << [count, word]
       end
-      @bigramlm[wordBack] = {}
-      @bigramlm[wordBack].default = {:tokencount => 0, :data => []}
-      @bigramlm[wordBack][:tokencount] = count
-      @bigramlm[wordBack][:data] = innerLM
+      @bigramlm[word_back] = {}
+      @bigramlm[word_back].default = {:tokencount => 0, :data => []}
+      @bigramlm[word_back][:tokencount] = count
+      @bigramlm[word_back][:data] = innerLM
     end
 
     count = 0
-    unigramcounts.each do |word, myCount|
+    unigram_counts.each do |word, myCount|
       count += myCount
       @unigramlm << [count, word]
     end
@@ -120,15 +120,15 @@ threelm[wordTwoBack][wordBack]
     true
   end
 
-  def checkVerbsAndComplementizers(sentenceSoFar, nextWord, probability = 0.3 )
+  def check_verbs_and_complementizers(sentence_so_far, next_word, probability = 0.3 )
     #implements "very shallow parsing" on a sentence
     #if the sentence doesn't have N + 1 verbs for every N complementizers, 
     #   then, with some probability, return false if the next word if it doesn't resolve the imbalance.
     #   and return false with higher probability if the next word increases the imbalance
     #   in hopes of finding a word that satisfies the imbalance on the next go-round.
 
-    tagged_sentence = @engtagger.add_tags( sentenceSoFar.join(" ") )
-    tagged_sentence_with_next_word = @engtagger.add_tags( (sentenceSoFar + [nextWord]).join(" ") )
+    tagged_sentence = @engtagger.add_tags( sentence_so_far.join(" ") )
+    tagged_sentence_with_next_word = @engtagger.add_tags( (sentence_so_far + [next_word]).join(" ") )
     last_word_tag_stuff = tagged_sentence_with_next_word.split("> <")[-1].gsub(/^</, "").gsub(/>$/, "")
     last_word_POS = last_word_tag_stuff[0...last_word_tag_stuff.index(">")]
     last_word_content = last_word_tag_stuff[last_word_tag_stuff.index(">")+1 ... last_word_tag_stuff.index("<")]
@@ -143,7 +143,7 @@ threelm[wordTwoBack][wordBack]
       return true
     end
 
-    puts [nextWord, @verb_count, @complementizer_count, last_word_POS].join(" ") if @debug
+    puts [next_word, @verb_count, @complementizer_count, last_word_POS].join(" ") if @debug
     if @complementizer_list.include? last_word_POS
       if @verb_count == @complementizer_count + 1 #balance
         true
@@ -181,148 +181,129 @@ threelm[wordTwoBack][wordBack]
     end
   end
 
-  def nextWordBigrams(wordBack)
-    nextWordChoicesStuff = @bigramlm[wordBack]
-    if rand < @unpathiness / Math.log( nextWordChoicesStuff[:tokencount])
+  def next_word_bigrams(word_back)
+    next_word_choices_stuff = @bigramlm[word_back]
+    if rand < @unpathiness / Math.log( next_word_choices_stuff[:tokencount])
       puts "backing off to unigrams." if @debug
-      nextWordChoicesStuff[:data] = @unigramlm
-      nextWordChoicesStuff[:tokencount] = @wordcount
+      next_word_choices_stuff[:data] = @unigramlm
+      next_word_choices_stuff[:tokencount] = @word_count
     end
-    nextWordChoicesStuff
+    next_word_choices_stuff
   end
-  def nextWordTrigrams(wordBack, wordTwoBack)
-    testthingy = @trigramlm[wordTwoBack]
-    nextWordChoicesStuff = testthingy[wordBack] || {:tokencount => 0, :data => []}
-    if rand < @unpathiness / Math.log( nextWordChoicesStuff[:tokencount])
+  def next_word_trigrams(word_back, word_two_back)
+    testthingy = @trigramlm[word_two_back]
+    next_word_choices_stuff = testthingy[word_back] || {:tokencount => 0, :data => []}
+    if rand < @unpathiness / Math.log( next_word_choices_stuff[:tokencount])
       puts "backing off to bigrams, aka smoothing." if @debug
-      nextWordChoicesStuff = nextWordBigrams(wordBack)
-    elsif ! nextWordChoicesStuff 
+      next_word_choices_stuff = next_word_bigrams(word_back)
+    elsif ! next_word_choices_stuff 
       puts "backing off to bigrams, since trigrams returned nothing" if @debug
-      nextWordChoicesStuff = nextWordBigrams(wordBack)
+      next_word_choices_stuff = next_word_bigrams(word_back)
     end
-    nextWordChoicesStuff
+    next_word_choices_stuff
   end
 
 
-  def getNextWord(wordBack, wordTwoBack)
-    #returns the next word based on the wordBack and wordTwoBack
-    #  dumb wrapper around nextWordBigrams, !!Trigrams functions.
+  def getnext_word(word_back, word_two_back)
+    #returns the next word based on the word_back and word_two_back
+    #  dumb wrapper around next_word_bigrams, !!Trigrams functions.
     #  i.e. doesn't do any parsing or anything.
-    nextWordChoicesStuff = nextWordTrigrams(wordBack, wordTwoBack)
-    if ! nextWordChoicesStuff 
+    next_word_choices_stuff = next_word_trigrams(word_back, word_two_back)
+    if ! next_word_choices_stuff 
       puts "backing off to bigrams, since trigrams returned nothing" if @debug
-      nextWordChoicesStuff = nextWordBigrams(wordBack)
+      next_word_choices_stuff = next_word_bigrams(word_back)
     end
-    nextWordChoices = nextWordChoicesStuff[:data].reverse
-    tokencount = nextWordChoicesStuff[:tokencount]
+    next_wordChoices = next_word_choices_stuff[:data].reverse
+    tokencount = next_word_choices_stuff[:tokencount]
 
-    nextWord = nil
+    next_word = nil
     myRand = rand(tokencount) + 1
-    puts nextWordChoices.inspect if @debug
+    puts next_wordChoices.inspect if @debug
     puts "rand: #{myRand}, count: #{tokencount}, unpathiness: #{@unpathiness * 100}%" if @debug
 
-    mostRecentWord = nil
-    nextWordChoices.each_with_index do | valword, index |
+    most_recent_world = nil
+    next_wordChoices.each_with_index do | valword, index |
       val,word = valword
       if (myRand == val) 
-        nextWord = word
+        next_word = word
         break
       elsif (myRand > val) 
-        nextWord = mostRecentWord 
+        next_word = most_recent_world 
         break     
-      elsif index == (nextWordChoices.size) - 1
-        nextWord = word
+      elsif index == (next_wordChoices.size) - 1
+        next_word = word
         break
       end
-      mostRecentWord = word
+      most_recent_world = word
     end
-    nextWord
+    next_word
   end
 
-  def getPhrase (opts = {})
+  def get_phrase (opts = {})
    o = { #set defaults
       :maxLen => 30, # if you set maxLen to 0, there is no maxLen
       :unpathiness => 0.0, #TODO: Remember what this does.
-      :wordTwoBack => "",
-      :wordBack => "", 
+      :word_two_back => "",
+      :word_back => "", 
       :debug => false,
       :engtagger => false, #TODO: Set to true, see if sentences are more coherent.
       :guaranteeParse => false,
    }.merge(opts)
 		@unpathiness = o[:unpathiness]
     maxLen = o[:maxLen]
-    wordTwoBack = o[:wordTwoBack]
-    wordBack = o[:wordBack]
+    word_two_back = o[:word_two_back]
+    word_back = o[:word_back]
     @debug = o[:debug]
     useEngtagger = o[:engtagger]
-    uselinkParserToValidateSentences = o[:guaranteeParse]  #should we return only parse-able sentences?
+    use_linkparser_to_validate_sentences = o[:guaranteeParse]  #should we return only parse-able sentences?
     puts "going to use engtagger" if @debug && useEngtagger
 
-    soFar = [wordTwoBack, wordBack]
+    so_far = [word_two_back, word_back]
     endOfSentence = false
 
     loop do
-      nextWord = getNextWord(wordBack, wordTwoBack)
-      if nextWord
+      next_word = getnext_word(word_back, word_two_back)
+      if next_word
         # conditions under which we reject a word and go back to picking another one.
-        if useEngtagger && ! checkVerbsAndComplementizers(soFar, nextWord)
-          puts "trying to resolve verb-complementizer imbalance, rejecting \"" + nextWord.to_s + "\"" if @debug
+        if useEngtagger && ! check_verbs_and_complementizers(so_far, next_word)
+          puts "trying to resolve verb-complementizer imbalance, rejecting \"" + next_word.to_s + "\"" if @debug
         elsif false #if the word is off topic
           #given a topic, 
           #check if an "on topic" word could come next -- with 10% chance, choose it.          
           
         else
-          soFar << nextWord
-          puts soFar.join(" ") if @debug
-          wordTwoBack = wordBack
-          wordBack = nextWord
-          break if nextWord == "" || (maxLen != 0 && soFar.length >= maxLen) 
+          so_far << next_word
+          puts so_far.join(" ") if @debug
+          word_two_back = word_back
+          word_back = next_word
+          break if next_word == "" || (maxLen != 0 && so_far.length >= maxLen) 
         end
       else
         break
       end
     end
-    sentence = soFar.join(" ").strip
+    sentence = so_far.join(" ").strip
 
-    if !uselinkParserToValidateSentences
+    if !use_linkparser_to_validate_sentences
       sentence
     else
       require 'linkparser'
-      sentence = soFar.join(" ").strip
+      sentence = so_far.join(" ").strip
       @linkparser = LinkParser::Dictionary.new
       result = @linkparser.parse(sentence)
       if result.linkages != []
         sentence
       else
         puts "ehhhhh, sentence didn't parse, trying again!" if @debug
-        getPhrase(opts) #if the sentence doesn't parse, throw it out and start again.
+        get_phrase(opts) #if the sentence doesn't parse, throw it out and start again.
       end
     end
   end
 
+
+  attr_reader :trigramlm, :bigramlm, :engtagger, :verb_count, :complementizer_count
   #accessor methods for debugging, etc.
-  def getDict (word)
-    @trigramlm[word]
-  end
-  def getBigramDict (word)
-    @bigramlm[word]
-  end
-  def getWholeBigramDict
-    @bigramlm
-  end
-  def getWholeDict
-    @trigramlm
-  end
-  def getEngtagger
-    @engtagger
-  end
-  def getVerbCount
-    @verb_count
-  end
-  def getComplementizerCount
-    @complementizer_count
-  end
 end
 
 @n = LanguageModel.new(["/home/merrillj/scotuslm/opinions", "*", "*", "*"], lambda{|filename| filename == "SCALIA.txt"})
-puts @n.getPhrase({:debug => true, :engtagger => true})
+puts @n.get_phrase({:debug => true, :engtagger => true})
--- opinionifier.rb
+++ opinionifier.rb
@@ -1,37 +1,37 @@
 # encoding: UTF-8
 require 'fileutils'
-require './lib/lineCombiner/lineCombiner.rb'
+require './lib/line_combiner/line_combiner.rb'
 require 'punkt-segmenter'
 
-def isBeginningOfOpinion(file, filename)
+def is_beginning_of_opinion(file, filename) #TODO: rename this method
   beginning = file.readlines[0..20]
   beginning.select!{|line| line != "" && ! /\d/.match(line)}
-  scotusLine = beginning.select {|line| !line.index("SUPREME COURT OF THE UNITED STATES").nil?} 
-  opinionLine = beginning.select {|line| !line.index("Opinion of").nil?} + beginning.select {|line| !line.index("concurring").nil?} +  beginning.select {|line| !line.index("dissenting").nil?} 
-  if opinionLine.size >= 1
-    justice = opinionLine.first.gsub("Opinion of ", "").strip
+  scotus_line = beginning.select {|line| !line.index("SUPREME COURT OF THE UNITED STATES").nil?} 
+  opinion_line = beginning.select {|line| !line.index("Opinion of").nil?} + beginning.select {|line| !line.index("concurring").nil?} +  beginning.select {|line| !line.index("dissenting").nil?} 
+  if opinion_line.size >= 1
+    justice = opinion_line.first.gsub("Opinion of ", "").strip
     if justice != "the Court"
       justice = justice.split(" ")[0].gsub(",","")
     end
   end
-  underscoreLine = beginning.select {|line| !line.index("__________").nil?}
-  caseLine = beginning.select {|line| !line.index("v.").nil?}
+  underscore_line = beginning.select {|line| !line.index("__________").nil?}
+  case_line = beginning.select {|line| !line.index("v.").nil?}
 
-  isSyllabus = !beginning.select {|line| !line.index("Syllabus").nil?}.empty?
+  is_syllabus = !beginning.select {|line| !line.index("Syllabus").nil?}.empty?
 
-  isBeginning = !scotusLine.empty? && !opinionLine.empty? && !underscoreLine.empty? && !caseLine.empty?
-  if isBeginning && false
-    puts scotusLine.inspect
-    puts opinionLine.inspect
-    puts underscoreLine.inspect
-    puts caseLine.inspect
+  is_beginning = !scotus_line.empty? && !opinion_line.empty? && !underscore_line.empty? && !case_line.empty?
+  if is_beginning && false
+    puts scotus_line.inspect
+    puts opinion_line.inspect
+    puts underscore_line.inspect
+    puts case_line.inspect
     puts "\n\n"
   end
 
-  [isBeginning, justice, isSyllabus]
+  [is_beginning, justice, is_syllabus]
 end
 
-def formatOpinion(opinion, opinionName, lineCombiner = LineCombiner.new)
+def format_opinion(opinion, opinion_name, line_combiner = LineCombiner.new)
   #cut out page numbers, etc.
   #remove a line if it is:
   # => in all caps
@@ -40,131 +40,131 @@ def formatOpinion(opinion, opinionName, lineCombiner = LineCombiner.new)
   # => is blank
   # => is all dashes
   # => is "v."
-  newOpinion = []
+  new_opinion = []
   footnotes = []
   opinion.each do |page|
-    newPage = []
-    isFootnotes = false
+    new_page = []
+    is_footnotes = false
     page.split("\n").each do |line|
       unless line == "" || /^\d+$/.match(line) || line.match(/^[0-9]+$/) || line.match(/Cite as: .*/) || \
           !line.index("________").nil? || !line.index("SUPREME COURT OF THE UNITED STATES").nil? || \
           !line.index("Opinion of").nil? || !line.match(/^[A-Z ,.]+ (concurring|dissenting)$/).nil?  ||\
           line.match(/^v.$/) || line.match(/^[A-Z., ]+$/) || !line.index("——————").nil? ||\
           !line.index("").nil? || line == ("Per Curiam") || line.match(/No\. \d+–\d+. Decided [A-Za-z]+ \d+, \d+/) ||\
-          line.match(/No\. \d+\d+ \[[A-Za-z]+ \d+, \d+\]/) || isFootnotes
-        newPage << line
+          line.match(/No\. \d+\d+ \[[A-Za-z]+ \d+, \d+\]/) || is_footnotes
+        new_page << line
       end
-      if !line.index("——————").nil? || isFootnotes
-        isFootnotes = true
+      if !line.index("——————").nil? || is_footnotes
+        is_footnotes = true
         footnotes << line
       end
     end
 
-    newPageCombined = newPage.reduce([]) do |lines, nextLine|
-      if !lines.empty? && !nextLine.nil? && nextLine.strip != ""
+    new_page_combined = new_page.reduce([]) do |lines, next_line|
+      if !lines.empty? && !next_line.nil? && next_line.strip != ""
         w1 = lines[-1].split(" ")[-1]
         raise AsdfError if w1.include? " "
-        w2 = nextLine.strip.split(" ")[0]
+        w2 = next_line.strip.split(" ")[0]
         w2suffix = ""
         if w2.include? ("'s")
           w2 = w2.split("'s")[0]
           w2suffix = "'s"
         end
-        restOfNextLine = nextLine.strip.split(" ")[1..-1]
+        rest_of_next_line = next_line.strip.split(" ")[1..-1]
         raise AsdfError if w2.length == 0
         if w2.match(/[A-Z][a-z]+/) #if w2 is capitalized, don't combine.
-          lines + [nextLine.strip]
-        elsif lineCombiner.safeToCombine?(w1, w2) 
+          lines + [next_line.strip]
+        elsif line_combiner.safe_to_combine?(w1, w2) 
           puts "combined: " + w1 + w2
           lines[-1] = lines[-1].split(" ")[0..-2] + [w1 + w2] #add w2 to the end of w1
-          lines + [restOfNextLine.strip]
+          lines + [rest_of_next_line.strip]
         else
-          lines + [nextLine.strip]
+          lines + [next_line.strip]
         end
       else
-        lines + [nextLine.strip]
+        lines + [next_line.strip]
       end
     end
 
-    newOpinion << newPageCombined.join(" ") #TODO split on punctuation and join('\n') 
+    new_opinion << new_page_combined.join(" ") #TODO split on punctuation and join('\n') 
   end
-  #newOpinion += footnotes
-  newOpinion = newOpinion.join(" ").gsub("[", "").gsub("]", "").gsub("(","").gsub("­\n","").gsub("­ ", "").gsub(")", "").gsub(" . . .", "...").gsub(/“|”/, '"')
-  if newOpinion.size > $largest[0]
-    $largest = [newOpinion.size, opinionName] 
-    puts newOpinion.size
+  #new_opinion += footnotes
+  new_opinion = new_opinion.join(" ").gsub("[", "").gsub("]", "").gsub("(","").gsub("­\n","").gsub("­ ", "").gsub(")", "").gsub(" . . .", "...").gsub(/“|”/, '"')
+  if new_opinion.size > $largest[0]
+    $largest = [new_opinion.size, opinion_name] 
+    puts new_opinion.size
   end
-  tokenizer = Punkt::SentenceTokenizer.new(newOpinion)
-  lines = tokenizer.sentences_from_text(newOpinion, :output => :sentences_text).join("\n")
+  tokenizer = Punkt::SentenceTokenizer.new(new_opinion)
+  lines = tokenizer.sentences_from_text(new_opinion, :output => :sentences_text).join("\n")
 end
 
 
-def formatAllOpinions(lineCombiner = LineCombiner.new)
-  yearFolders = Dir["txts/*"]
-  yearFolders.each do |folder|
+def format_all_opinions(line_combiner = LineCombiner.new)
+  year_folders = Dir["txts/*"]
+  year_folders.each do |folder|
     cases = Dir.glob("#{folder}/*")
-    cases.each do |caseFolder|
-      pages = Dir.glob("#{caseFolder}/*")
-      endOfMajorityOpinionPages = []
-      orderOfJustices = []
+    cases.each do |case_folder|
+      pages = Dir.glob("#{case_folder}/*")
+      end_of_majority_opinion_pages = []
+      order_of_justices = []
       pages.each do |filename|
         number = filename.split("_")[-1].split(".")[0]
         rest = filename.split("_")[0..-2].join("") 
         if number.length == 3
-          newNumber = number
+          new_number = number
         elsif number.length == 2
-          newNumber = "0" + number
+          new_number = "0" + number
         elsif number.length == 1
-          newNumber = "00" + number
+          new_number = "00" + number
         else
           puts number, filename
           raise AsdfError
         end
-        newFilename = [rest, "_", newNumber, ".txt"].join("")
+        newFilename = [rest, "_", new_number, ".txt"].join("")
         FileUtils.move(filename, newFilename) unless filename == newFilename
       end
       opinion = []
-      orderedPages = Dir.glob("#{caseFolder}/*").sort
-      orderedPages.each_with_index do |filename, index|
-        pageNumber = filename[(filename.rindex("_")+1)..-5].to_i
+      ordered_pages = Dir.glob("#{case_folder}/*").sort
+      ordered_pages.each_with_index do |filename, index|
+        page_number = filename[(filename.rindex("_")+1)..-5].to_i
         #outputFilename = filename.gsub("txt", "opinion")
         f = File.open(filename)
-        isBeginning = isBeginningOfOpinion(f, filename)
+        is_beginning = is_beginning_of_opinion(f, filename)
         f.close()
         f = File.open(filename)
-        if isBeginning[2] && orderOfJustices == []
-          orderOfJustices << "syllabus"
+        if is_beginning[2] && order_of_justices == []
+          order_of_justices << "syllabus"
         end
-        if isBeginning[0] || index == pages.size-1
+        if is_beginning[0] || index == pages.size-1
           puts "asfd" if index == cases.size-1
-          justice = isBeginning[1]
-          endOfMajorityOpinionPages.push(pageNumber)
-          orderOfJustices.push(justice)
+          justice = is_beginning[1]
+          end_of_majority_opinion_pages.push(page_number)
+          order_of_justices.push(justice)
           if justice
             justice = justice.split(",")[0].strip()
           else
             justice = "only"
           end
           opinion << f.read() if index == pages.size-1
-          previousJustice = orderOfJustices[-2] || "per curiam" 
-          opinionFilepath = File.join("opinions", caseFolder.split("/")[1], caseFolder.split("/")[2], previousJustice) + ".txt"
-          rawOpinionFilepath = File.join("opinions", caseFolder.split("/")[1], caseFolder.split("/")[2], previousJustice) + ".raw.txt"
-          FileUtils.mkdir_p(File.dirname(opinionFilepath)) if ! File.exists? File.dirname(opinionFilepath)
+          previous_justice = order_of_justices[-2] || "per curiam" 
+          opinion_filepath = File.join("opinions", case_folder.split("/")[1], case_folder.split("/")[2], previous_justice) + ".txt"
+          raw_opinion_filepath = File.join("opinions", case_folder.split("/")[1], case_folder.split("/")[2], previous_justice) + ".raw.txt"
+          FileUtils.mkdir_p(File.dirname(opinion_filepath)) if ! File.exists? File.dirname(opinion_filepath)
           begin
-            File.open(opinionFilepath, "w") {|f| f.write(formatOpinion(opinion, opinionFilepath, lineCombiner)) }
+            File.open(opinion_filepath, "w") {|f| f.write(format_opinion(opinion, opinion_filepath, line_combiner)) }
           rescue NoMethodError
             puts filename
           end
-          File.open(rawOpinionFilepath, "w") {|f| f.write(opinion.join("\n")) }
+          File.open(raw_opinion_filepath, "w") {|f| f.write(opinion.join("\n")) }
           opinion = []
         end
         opinion << f.read()
       end
-      puts caseFolder + ": " + endOfMajorityOpinionPages.inspect + "; " + orderOfJustices.inspect
+      puts case_folder + ": " + end_of_majority_opinion_pages.inspect + "; " + order_of_justices.inspect
       #now we know where the
     end
   end
 end
 $largest = [0, ""]
-formatAllOpinions()
+format_all_opinions()
 puts $largest
