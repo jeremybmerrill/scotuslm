7938ea11baf7b0c32ec306962a35fe72419dd897
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2012-10-18 23:17:11 -0400

    stuff works, I guess

--- LanguageModel.py
+++ LanguageModel.py
@@ -23,6 +23,7 @@ class LanguageModel:
 
 
   def __init__(self, pathglob, filesFilter = lambda x: True ):
+    self.topic_similarity = None
     trigram_counts = dict()
     unigram_counts = dict()
     bigram_counts = dict() #e.g. {San => {Francisco => 50, Jose => 20} }
@@ -30,20 +31,23 @@ class LanguageModel:
     #complementizer_list = ['AZ', 'TH', 'TS', ]
     #verb_list = ['S']
     self.complementizer_list = ["WP", "WPS," "WDT", "WRB"] #also, "in", dealt with separately
-    #actual_IN_complementizers = ["that", "although", "after", "although", "if", "unless", "as", "inasmuch", "until", "when", "whenever", "since", "while", "because", "before", "though"]
+    self.actual_IN_complementizers = ["that", "although", "after", "although", "if", "unless", "as", "inasmuch", "until", "when", "whenever", "since", "while", "because", "before", "though"]
     self.verb_list = ["VB", "VBD", "VBP", "VBZ",] #, "vbg", "vbn" #participles, which I don't really want.
     #complementizer_count = 0
     #verb_count = 0
     self.makes_things_worse_adjustment = 3 #if the selected part of speech would make things worse, multiply this by probability to make it harder to add the selected word
-    self.unpathiness = 0
+    self.unpathiness = 0.2
     self.debug = False
+    self.RANDOM_WEIGHT = 1
+    self.TOPICALITY_WEIGHT = 1
+    self.VERY_SHALLOW_PARSE_WEIGHT = 1
+    self.word_count = 0
 
-    word_count = 0
     files = glob.glob(os.path.join(*pathglob)) #TODO
     for f in files:
       if os.path.basename(f) == '.' or os.path.basename(f) == '..' or not filesFilter(os.path.basename(f)):
         continue
-      for sentence in codecs.open(f, "r", "utf-8"): #files are pre-split into sentences
+      for sentence in codecs.open(f, "r", encoding = "utf-8"): #files are pre-split into sentences
         #line = line.split(/[.?!]|$/) 
         split_sentence = nltk.word_tokenize(sentence.strip())
         word_two_back = ""
@@ -51,7 +55,7 @@ class LanguageModel:
         for word in split_sentence:
           #word.replace("—", "")
           word = re.sub(re.compile("[_\(\),]"), "", word.lower() ) #—
-          word_count += 1
+          self.word_count += 1
 
           unigram_counts[word] = unigram_counts.get(word, 0) + 1
           bigram_inner_dict = copy.copy(bigram_counts.get( word_back, dict()))
@@ -105,75 +109,80 @@ class LanguageModel:
     if self.debug:
       print("done generating hashes")
 
-  def check_verbs_and_complementizers(self, sentence_so_far, next_word, probability = 0.3 ):
-    #implements "very shallow parsing" on a sentence
-    #if the sentence doesn't have N + 1 verbs for every N complementizers, 
-    #   then, with some probability, return False if the next word if it doesn't resolve the imbalance.
-    #   and return False with higher probability if the next word increases the imbalance
-    #   in hopes of finding a word that satisfies the imbalance on the next go-round.
+  def very_shallow_parse_score(self, sentence_so_far, next_word):
+    """ return a value representing whether it will make the sentence more or less coherent via "very shallow parsing"
 
-    #TODO: rewrite to return a probability of rejection.
+    Return: integer 0 < x < 1. 1 represents that the word makes the much sentence more coherent, 0 that it makes it much worse.
 
-    tagged_sentence_so_far = nltk.pos_tag(nltk.word_tokenize( " ".join(sentence_so_far) ))
+    if the sentence doesn't have N + 1 verbs for every N complementizers, 
+      then, return a low score if the next word doesn't resolve the imbalance.
+      and return an even lower score if the next word increases the imbalance
+      in hopes of prioritizing words that will cause the sentence to be more
+      coherent.
+    """
+    tagged_sentence_so_far = nltk.pos_tag(nltk.word_tokenize( " ".join(sentence_so_far) )) 
     tagged_sentence_with_next_word =  nltk.pos_tag(nltk.word_tokenize( " ".join(sentence_so_far + [next_word]) ))
     next_word_POS = tagged_sentence_with_next_word[-1][1] #the next word's tag is the second element of the final element of the list of words in the sentence.
     print(str(tagged_sentence_with_next_word ))
     #TODO: what if the sentence didn't get tagged successfully?
     verb_count = len([pos for word, pos in tagged_sentence_so_far if pos in self.verb_list])
-    complementizer_count = len([pos for word, pos in tagged_sentence_so_far if pos in self.complementizer_list]) #TODO: some complementizers are BS.
+    complementizer_count = len([pos for word, pos in tagged_sentence_so_far if pos in self.complementizer_list]) and word in self.actual_IN_complementizers #TODO: some complementizers are BS.
+
+    RESOLVES_IMBALANCE = 1.0
+    FURTHERS_IMBALANCE = 0.0
+    BALANCE = 1.0
+    IMBALANCE_NOT_A_VERB_OR_COMPLEMENTIZER = 0.7
 
     if self.debug:
-      print(". ".join(map(str, [next_word, verb_count, complementizer_count, next_word_POS])).encode("utf-8"))
+      try:
+        print(". ".join(map(unicode, [next_word, verb_count, complementizer_count, next_word_POS])).encode("utf-8"))
+      except UnicodeEncodeError:
+        print "eh, tried to print, but error. :("
 
     if verb_count == 0 and complementizer_count == 0:
-      return True
+      return BALANCE #anything's fine, whatever. #TODO: constantize these magic-number values.
 
-    if next_word_POS in self.complementizer_list:
+    #Complementizer
+    if next_word_POS in self.complementizer_list and next_word in self.actual_IN_complementizers:
       if verb_count == complementizer_count + 1: #balance
-        return True
+        return BALANCE
       elif verb_count > complementizer_count + 1: #imbalance -- complementizer needed
         #accept, this fixes the imbalance!
-        return True
-      elif random.random() > probability * self.makes_things_worse_adjustment: #verb needed, reject with high probability; allowing this word would increase the imbalance
-        #oh well, keep it.
-        return True
-      else:
-        return False
+        return RESOLVES_IMBALANCE
+      else:  #verb needed, this word would increase the imbalance
+        return FURTHERS_IMBALANCE
+    #Verb
     elif next_word_POS in self.verb_list:
       if verb_count == complementizer_count: #balance, feel free to take me out of balance verbly. :)
-        return True
+        return BALANCE
       elif verb_count < complementizer_count:  #imbalance -- verb needed
         #accept, this fixes the imbalance!
-        return True
-      elif random.random() > probability * self.makes_things_worse_adjustment: #complementizer needed, reject with high probability; allowing this word would increase the imbalance
-        #oh well, keep it.
-        return True
-      else:
-        return False
-    else:
+        return RESOLVES_IMBALANCE
+      else: #complementizer needed, this word would increase the imbalance
+        return FURTHERS_IMBALANCE
+    #Something else
+    else: #if it's some other word (e.g. a noun)
       if verb_count == complementizer_count + 1: #balance
-        return True
-      elif random.random() > probability: #selected word doesn't fix the imbalance.
-        #oh well, keep it.
-        return True
-      else:
-        #rejected!
-        return False
+        return BALANCE
+      else: #imbalance, this word doesn't help or make things worse.
+        return IMBALANCE_NOT_A_VERB_OR_COMPLEMENTIZER
+
 
   def next_word_bigrams(self,word_back):
     get_stuff = self.bigramlm.get(word_back, dict({"tokencount" : 0, "data" : []}))
     logthis = float("inf") if get_stuff.get("tokencount", 0) <= 1 else get_stuff["tokencount"]
-    if random.random() < self.unpathiness / Math.log( logthis ):
+    if random.random() < self.unpathiness / math.log( logthis ):
       if self.debug: 
         print("backing off to unigrams.")
-      get_stuff["data"] = unigramlm
-      get_stuff["tokencount"] = word_count
+      get_stuff["data"] = self.unigramlm
+      get_stuff["tokencount"] = self.word_count
     return get_stuff
 
   def next_word_trigrams(self, word_back, word_two_back):
     testthingy = self.trigramlm.get(word_two_back, dict({"tokencount" : 0, "data" : []}) )
     get_stuff = testthingy.get(word_back, dict({"tokencount" : 0, "data" : []}))
 
+    #TODO move unpathiness's backoff into get_next_word
     logthis = float("inf") if get_stuff.get("tokencount", 0) <= 1 else get_stuff["tokencount"]
     if random.random() < self.unpathiness / math.log( logthis ):
       if self.debug:
@@ -202,9 +211,9 @@ class LanguageModel:
 
     next_word = None
     myRand = random.randint(0, tokencount)  #TODO, double check output here.
-    if self.debug:
-      print(next_word_choices)
-      print("rand: " + str(myRand) + ", count: " + str(tokencount) + ", unpathiness: "+ str(self.unpathiness * 100))
+    # if self.debug:
+    #   print(next_word_choices)
+    #   print("rand: " + str(myRand) + ", count: " + str(tokencount) + ", unpathiness: "+ str(self.unpathiness * 100))
 
     most_recent_word = None
     for index, valword in enumerate(next_word_choices):
@@ -223,52 +232,64 @@ class LanguageModel:
 
   def get_phrase (self, **kwargs):
     #defaults
-    self.unpathiness = kwargs.get("unpathiness", 0.0)
-    maxLen = kwargs.get("maxLen", 30)  # if you set maxLen to 0, there is no maxLen
+    self.unpathiness = kwargs.get("unpathiness", self.unpathiness)
+    max_sentence_length = kwargs.get("max_sentence_length", 30)  # if you set max_sentence_length to 0, there is no max_sentence_length
     word_two_back = kwargs.get("word_two_back", "")
     word_back = kwargs.get("word_back", "")
     self.debug = kwargs.get("debug", False)
-    useShallowParsing = kwargs.get("veryShallowParse", False)
-    only_parseable_sentences = kwargs.get("guaranteeParse", False)  #should we return only parse-able sentences?
-    similar_words = kwargs.get("similar_words", None)
+    use_topic = kwargs.get("topic", False)
+    use_very_shallow_parsing = kwargs.get("veryShallowParse", False) 
+    only_parseable_sentences = kwargs.get("guaranteeParse", False)  #should we return only parse-able sentences? NotImplementedYet
 
-    if self.debug and useShallowParsing:
+    if self.debug and use_very_shallow_parsing:
       print("going to use very shallow parsing")
+
+    if "topic" in kwargs:
+      print "building topic similarity engine..."
+      import TopicSimilarity
+      if not self.topic_similarity:
+        self.topic_similarity = TopicSimilarity.TopicSimilarity(glob = ["/home/merrillj/scotuslm/opinions", "*", "*", "*"])
 
     so_far = [word_two_back, word_back]
     endOfSentence = False
 
     while 1:
-      next_word = self.get_next_word(word_back, word_two_back)
-      if next_word:
-        # conditions under which we reject a word and go back to picking another one.
-        #TODO: rewrite topics and very-shallow-parsing to each return a "how much we want/don't-want this word", to be run over a handful of words.
-        if useShallowParsing and not self.check_verbs_and_complementizers(so_far, next_word):
-          if self.debug: 
-            print("trying to resolve verb-complementizer imbalance, rejecting \"" + next_word + "\"" )
-        elif False: #if the word is off topic
-          #given a topic, 
-          #check if an "on topic" word could come next -- with 10% chance, choose it.     
-          pass     
-        else:
-          so_far.append(next_word)
-          if self.debug: 
-            print(" ".join(so_far))
-          word_two_back = word_back
-          word_back = next_word
-          if next_word == "" or (maxLen != 0 and len(so_far) >= maxLen):
-            break 
-      """
-      candidate_next_words = []
+      candidate_next_words = {}
       for i in range(10):
-        random_factor = .5
-        candidate_next_words.append([self.get_next_word(word_back, word_two_back), random.random() * random_factor])
-        for word in candidate_next_words:
-          #adjust scores based on topicality and veryShallowParse results.
-        #pick the highest ranked one
-      """
-      else:
-        break
+        #N.B.: The absolute values of the weights don't matter, what matters is the ratio between the three.
+        random_weight = kwargs.get("random_weight", self.RANDOM_WEIGHT)
+        topicality_weight = kwargs.get("topicality_weight", self.TOPICALITY_WEIGHT)
+        very_shallow_parse_weight = kwargs.get("very_shallow_parse_weight", self.VERY_SHALLOW_PARSE_WEIGHT)
+        #TODO: make these into settleable parameters.
+
+        word = self.get_next_word(word_back, word_two_back)
+
+        word_score = random.random() * random_weight
+        if word:
+          if use_topic and self.topic_similarity:
+            word_score += ( self.topic_similarity.similar_to(kwargs["topic"], word) ) * topicality_weight
+          if use_very_shallow_parsing:
+            word_score += self.very_shallow_parse_score(so_far, word) * very_shallow_parse_weight
+        candidate_next_words[word] = word_score
+        #TODO: This weights (unnecessarily) towards continuing a sntence, rather than ending it. Fix this.
+
+      next_word = ""
+      max_word_score = 0.0
+      for word, word_score in candidate_next_words.items():
+        if word_score > max_word_score:
+          print " ".join([u'"' + unicode(word) + u'"', "superseding", u'"' + unicode(next_word) + u'"', u"with a score of", str(word_score)]).encode('utf-8')
+          next_word = word
+          max_word_score = word_score
+      if not next_word:
+        next_word = ""
+      so_far.append(next_word)
+      if self.debug: 
+        print so_far
+        print(" ".join(so_far))
+      word_two_back = word_back
+      word_back = next_word
+      if next_word == "" or (max_sentence_length != 0 and len(so_far) >= max_sentence_length):
+        break 
     sentence = " ".join(so_far).strip()
 
     if not only_parseable_sentences:
@@ -287,20 +308,26 @@ class LanguageModel:
         return get_phrase(opts) #if the sentence doesn't parse, throw it out and start again.
       """
 
-  def get_paragraph(self, length = 10, **kwargs):
+  def get_paragraph(self, **kwargs):
     paragraph = []
+    length = kwargs.get("paragraph_length", 10)
+    if "paragraph_length" in kwargs:
+      del kwargs["paragraph_length"]
+
     opts = kwargs
-    if "topic" in kwargs:
-      print "building topic similarity engine..."
-      import TopicSimilarity
-      if not self.topic_similarity:
-        self.topic_similarity = TopicSimilarity.TopicSimilarity(glob = ["/home/merrillj/scotuslm/opinions", "*", "*", "*"])
-      similar_words = self.topic_similarity.similar_to(kwargs["topic"], max=20)
-      opts["similar_words"] = similar_words
+
     for i in range(length):
-      paragraph.append(self.get_phrase(**opts))
-    return " ".join(paragraph)
+      next_sentence = self.get_phrase(**opts)
+      if next_sentence:
+        #cleanup
+        if next_sentence[-1] == ".":
+          next_sentence = next_sentence[0:-1]
+        if next_sentence[-1] == " ":
+          next_sentence = next_sentence[0:-1]
+        paragraph.append(next_sentence.capitalize())
+    return u".\n ".join(paragraph)
 
 myLM = LanguageModel(["/home/merrillj/scotuslm/opinions", "*", "*", "*"], lambda filename: filename == "SCALIA.txt" )
-opts = dict({"debug" : True, "veryShallowParse" : True, "maxLen" : 50})
-print(myLM.get_phrase( opts ))
+opts = dict({"debug" : True, "veryShallowParse" : True, "max_sentence_length" : 50, "paragraph_length" : 10, 
+  'topic':'congress', 'random_weight': 1, 'topicality_weight':3, 'very_shallow_parse_weight':3})
+print(myLM.get_phrase( **opts )).encode("utf-8")
--- TopicSimilarity.py
+++ TopicSimilarity.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-#TODO: turn self.tfidfs into numpy vectors.
+#TODO: turn self.tfidfs into numpy sparse vectors.
+#TODO: when dumping, record glob and filesFilter in a separate "manifest" file, so when the same glob and filesFilter are supplied, we don't need to regenerate.
+#TODO: Explore other methods of TFIDF (e.g. normalized)
 
 import numpy
 import codecs
@@ -9,6 +11,7 @@ import glob
 import os
 import nltk
 import math
+from scipy.spatial.distance import cosine
 
 class TopicSimilarity:
   """ 
@@ -27,7 +30,6 @@ class TopicSimilarity:
   """
   TODO: 
     - profile to check efficiency of side-effecty or add'l iteration for DF.
-    - downcase stuff
 
   Next steps:
     - From TFIDFs, put each word into N-space. That is, order the words in the TFIDF dict (having been filtered, I guess).
@@ -39,10 +41,25 @@ class TopicSimilarity:
     Create this vector shit in memory. I guess I've got to do it anyways, so I might as well see how fast it is...
   """
 
-  def __init__(self):
+  default_dump_path = "TopicSimilarityDump.dat"
+
+  def __init__(self, **kwargs):
     self.tfidfs = {}
     self.puncts = [",", ".", ";", ":", "!", "?", "-", u"’", "\"", ]
     self.stoplist = open("english.stop.txt").read().split("\n")
+    if "infile" in kwargs:
+      self.load(kwargs["infile"])
+    elif "glob" in kwargs:
+      if "filesFilter" in kwargs:
+        self.generate_tfidfs(kwargs["glob"], kwargs["filesFilter"])
+      else:
+        self.generate_tfidfs(kwargs["glob"])
+    elif os.path.exists("TopicSimilarityDump.dat"):
+      print "loading from default dump path."
+      self.load("TopicSimilarityDump.dat")
+    else:
+      raise TypeError('Required keyword argument infile or glob not found.')
+
     #stoplist via:  http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop
 
   def tf(self, text_path):
@@ -51,18 +68,28 @@ class TopicSimilarity:
     terms_dict = {}
     for line in text.readlines():
       for word in nltk.word_tokenize(line):
+        word = word.lower()
         if word not in self.puncts and word not in self.stoplist:
           terms_dict[word] = terms_dict.get(word, 0) + 1
     return terms_dict
 
-  def generate_tfidfs(self, pathglob, lambdaFunc = lambda x: True ):
+  def generate_tfidfs(self, pathglob, filesFilter = lambda x: True ):
+    """ For words in documents specified in pathglob and filesFilter, construct TFIDF vectors.
+
+    Each word's TFIDF vector contains its TFIDF for a given document. 
+
+    The correspondence between a document's index in a word's TFIDF vector and its name is not consistent, but it does remain
+      constant within calls of this method. Thus, words with similar TFIDFs for a given index in the vector share some similarity.
+
+    This method is side-effecty; self.tfidfs contains the TFIDF vectors. This method doens't return anything.
+    """
     tfs = {}
     df = {}
     temp_tfidfs = {}
     document_paths = glob.glob(os.path.join(*pathglob)) #TODO
     number_of_documents = len(document_paths)
     for document_path in document_paths:
-      if os.path.basename(document_path) == '.' or os.path.basename(document_path) == '..' or not lambdaFunc(os.path.basename(document_path)):
+      if os.path.basename(document_path) == '.' or os.path.basename(document_path) == '..' or not filesFilter(os.path.basename(document_path)):
         continue
       tfs[document_path] = self.tf(document_path)
     #Lots of possibilities for creating DF dict.
@@ -73,22 +100,77 @@ class TopicSimilarity:
       for word in tf.keys():
         df[word] = df.get(word, 0) + 1 #document frequency.
     for key, tf in tfs.items():
-      for word, count in tf.items():
-        if key not in temp_tfidfs:
-          temp_tfidfs[key] = {}
-        key_word_tfidf = float(count) * math.log(float(number_of_documents) / df[word])
-        if key_word_tfidf > 40:
-          temp_tfidfs[key][word] = key_word_tfidf
-    for key, tfidf_dict in temp_tfidfs.items():
-      self.tfidfs[key] = numpy.array(sorted(tfidf_dict.items(), key=lambda x:x[1]))
+      temp_tfidfs[key] = {}
+      for word, count in tf.items():         
+        filepath_word_tfidf = float(count) * math.log(float(number_of_documents) / df[word])
+    #     if filepath_word_tfidf > 40:
+    #       temp_tfidfs[key][word] = filepath_word_tfidf
+    # for key, tfidf_dict in temp_tfidfs.items():
+    #   self.tfidfs[key] = numpy.array(sorted(tfidf_dict.items(), key=lambda x:x[1]))
+        temp_tfidfs[key][word] = filepath_word_tfidf
+      #need to pivot documents to be {word : ordered_vector_of_tfidf_by_document}
+    for index, docs_to_tfidfs in enumerate(temp_tfidfs.items()):
+      filename, tfidfs = docs_to_tfidfs #I don't care what document a TFIDF came from, as long as its consistent.
+      for word, tfidf in tfidfs.items():
+        self.tfidfs[word] = self.tfidfs.get(word, numpy.zeros( (1, len(temp_tfidfs)))[0])
+        self.tfidfs[word][index] = tfidf
+
+  def similar_words(self, word, max=None):
+    """ Returns *max* tuples of similarities (0 to 1), words similar to *word*. """
+    similarity = []
+    for candidate_word, tfidfs in self.tfidfs.items():
+      if candidate_word == word: #words don't count as similar to themselves
+        continue
+      try:
+        distance = cosine_distance(self.tfidfs.get(word.lower()), self.tfidfs[candidate_word])
+      except KeyError:
+        distance = float('inf')
+
+      similarity.append((distance, candidate_word)) 
+      #TODO: remove the word itself.
+    if max:
+      return sorted(filter(lambda x: x[0] > 0.2, similarity), reverse=True)[0:max]
+    else:
+      return sorted(filter(lambda x: x[0] > 0.2, similarity), reverse=True)
+
+  def similar_to(self, word1, word2):
+    """Returns w2's similarity to w1 as float, 0 < n < 1. """
+    if word1 in self.stoplist or word2 in self.stoplist or word1 not in self.tfidfs or word2 not in self.tfidfs:
+      return 0.01 #why? I dunno. 
+    else:
+      return cosine_distance(self.tfidfs[word1.lower()], self.tfidfs[word2.lower()])
 
   def load(self, infile="TopicSimilarityDump.dat"):
     """Load from a dump."""
+    import cPickle
+    self.tfidfs = cPickle.load(open(infile))
+
 
   def dump(self, outfile="TopicSimilarityDump.dat"):
-    """Dump the language similarity stuff, so you don't need to regenerate it every time. Maybe via pickle or shelve or something?""" 
+    """Dump the language similarity data.""" 
+    import cPickle
+    cPickle.dump(self.tfidfs, open(outfile))
 
-global t
-t = TopicSimilarity()
-t.generate_tfidfs(["/home/merrillj/scotuslm/opinions", "*", "*", "*"], lambda filename: filename == "SCALIA.txt" )
-print t.tfidfs[t.tfidfs.keys()[6]]
+def cosine_distance(u, v):
+  """
+  Returns the cosine of the angle between vectors v and u. This is equal to
+  u.v / |u||v|.
+
+  via: http://stackoverflow.com/questions/2380394/simple-implementation-of-n-gram-tf-idf-and-cosine-similarity-in-python
+  """
+  return numpy.dot(u, v) / (math.sqrt(numpy.dot(u, u)) * math.sqrt(numpy.dot(v, v))) 
+
+"""
+#NB: only for profiling
+import time
+
+before = time.time()
+print time.localtime(before)
+t = TopicSimilarity() # 103.504664898secs
+#t = TopicSimilarity(glob = ["/home/merrillj/scotuslm/opinions", "*", "*", "*"] ) #337.698110104secs
+#t = TopicSimilarity(glob = ["/home/merrillj/scotuslm/opinions", "*", "*", "*"], filesFilter= lambda filename: filename == "SCALIA.txt" ) #crappy results, 6.03190803528secs
+print str(time.time() - before) + "secs"
+print time.localtime()
+
+print t.similar_words("habeas")[0:10]
+"""
