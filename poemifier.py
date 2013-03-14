2730f1921eecc23b8154ee26d4a456ae1b87acd3
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2013-03-13 22:37:58 -0400

    works (except for split lines) with much faster algorithm

--- poemifier.py
+++ poemifier.py
@@ -3,17 +3,17 @@ from random import random
 
 """
 #TODO:
-Architecture changes:
-  1. Give poemifier the whole document. (Eventually: chunks of document, with reprocessing at each chunk.)
-  2. Create two "hash" dicts, one for rhymes and one for syllable length.
+Architecture:
+  1. For each validation type (e.g. syllable count, rhyming, eventually stress), create a "hash" dicts.
     a. Each dict is of the form: hash_value -> [line1, line2, line3]
     b. each hash_value is computed by hash(line) where the syllable_hash function returns syllable length of the line and rhyme_hash returns the rime (i.e. nucleus + coda) of the final syllable of the line.
-  3. Discard or ignore all syllable_hash_dict entries whose syllable counts are not in the format.
-  3.5: Optionally, pair stuff, e.g. rhyming pairs or triplets (or n-lets where n is the greatest amount of rhyming lines needed in the format, e.g. 3 for a limerick, , but only 2 for a sonnet.) and, if necessary, of the right syllable length.
-  3.75 From these pairs, assemble a poem.
-  4. iterate over each line of the right syllable length to see if anything rhymes with it (i.e. has the same rhyme_hash value) and if so, insert into the poem. (with the same tricks to keep options open in self.poems)
+  2. Discard or ignore all syllable_hash_dict entries whose syllable counts are not in the format.
+  3: Optionally, pair stuff, e.g. rhyming pairs or triplets (or n-lets where n is the greatest amount of rhyming lines needed in the format, e.g. 3 for a limerick, , but only 2 for a sonnet.) and, if necessary, of the right syllable length.
+  4. From these pairs, assemble a poem.
 """
 
+#TODO: use espeak, add pitch to "sing" the poems
+#TODO: fix up rhymechecker to check based on penultimate rime in words with penultimate stress.
 class Poemifier:
   def __init__(self, format_name, format=None):
     """Specify the name of a known format or specify a fully-defined format."""
@@ -61,23 +61,34 @@ class Poemifier:
       if multiple % 1.0 != 0.0:
         raise TypeError, "Invalid poem format :("
       format["rhyme_scheme"] = int(multiple) * format["rhyme_scheme"]
-    format["syllable_counts_needed"] = []
+    format["unique_syllable_structure"] = set()
     for syllable_count_item in format["syllable_structure"]:
-      if isinstance(syllable_count_item, int) and syllable_count_item not in format["syllable_counts_needed"]:
-         format["syllable_counts_needed"].append(syllable_count_item)
-      else:
-        for num in range([line_index][0],[line_index][1]+1):
-          if num not in format["syllable_counts_needed"]:
-            format["syllable_counts_needed"].append(num)
+      format["unique_syllable_structure"].add(syllable_count_item)
+    format["syllable_count_to_syllable_count_token"] = {}
+
+    for syllable_count_item in format["syllable_structure"]:
+      if isinstance(syllable_count_item, int):
+        if syllable_count_item not in format["syllable_count_to_syllable_count_token"].keys():
+          format["syllable_count_to_syllable_count_token"][syllable_count_item] = set()
+        format["syllable_count_to_syllable_count_token"][syllable_count_item].add(syllable_count_item)
+      elif isinstance(syllable_count_item, tuple):
+        for num in range(syllable_count_item[0],syllable_count_item[1]+1):
+          if num not in format["syllable_count_to_syllable_count_token"].keys():
+            format["syllable_count_to_syllable_count_token"][num] = set()
+          format["syllable_count_to_syllable_count_token"][num].add(syllable_count_item)
     return format
 
   def _rime(self, line):
-    #return this line's last word's rime to use as key to this value in the rhyme hash.
+    """Return this line's last word's rime to use as key to this value in the rhyme hash."""
     #TODO integrate with rhymechecker's stress stuff to sometimes return penultimate syllable's rime + last syllable.
-    return tuple(self.rhyme_checker.get_rime(line.split(" ")[-1]))
+    rime = self.rhyme_checker.get_rime(line.split(" ")[-1])
+    if rime:
+      return tuple(rime)
+    else:
+      False
 
   def _syllable_count(self, line):
-    #return this line's syllable count to use as key to this value in the syllable_count hash.
+    """return this line's syllable count to use as key to this value in the syllable_count hash."""
     return sum(map(self.rhyme_checker.count_syllables, line.split(" ")))
 
   def try_line(self, line):
@@ -85,7 +96,6 @@ class Poemifier:
     #TODO: add line to all hashes.
     self.add_line(line)
     #TODO: split line here.
-    #TODO: do any intermediate processing (e.g. pairing rhyming lines)
     return self.get_poem() #False or a poem.
 
   def add_line(self, line):
@@ -96,95 +106,24 @@ class Poemifier:
     # format_items = [[self._rime, self.rhyme_dict], [self._syllable_count, self.syllable_count_dict]]
     # for hashFunc, format_hash_dict in format_items:
     syll_count = self._syllable_count(line)
-    if syll_count in self.format["syllable_counts_needed"]: #TODO: obviously, abstract this.
+    if syll_count in self.format["syllable_count_to_syllable_count_token"].keys(): #TODO: obviously, abstract this.
       rime = self._rime(line)
-      if rime not in self.rhyme_dict:
-        self.rhyme_dict[rime] = []
-      self.rhyme_dict[rime].append(line)
-      if syll_count not in self.syllable_count_dict:
-        self.syllable_count_dict[syll_count] = []
-      self.syllable_count_dict[syll_count].append(line)
+      if not rime:
+        return False
+
+      for syll_count_token in self.format["syllable_count_to_syllable_count_token"][syll_count]:
+        if syll_count_token not in self.syllable_count_dict:
+          self.syllable_count_dict[syll_count_token] = []
+        self.syllable_count_dict[syll_count_token].append(line)
+
+        if rime not in self.rhyme_dict:
+          self.rhyme_dict[rime] = {}
+        if syll_count_token not in self.rhyme_dict[rime]:
+          self.rhyme_dict[rime][syll_count_token] = []
+        self.rhyme_dict[rime][syll_count_token].append(line)
       return True
     else:
       return False
-
-  # def old_try_line(self, line):
-  #   #TODO: reorganize the finished output to maximize topicality between adjacent lines.
-  #   if self.debug:
-  #     if len(self.poems) % 10 == 0:
-  #       print len(self.poems)
-  #     if len(self.poems) % 10 == 0:
-  #       print self.poems.sort(key= lambda x: x.count(None))
-  #   extra_poems = []
-  #   for poem_index, temp_poem in enumerate(self.poems):
-  #     for line_index, poem_line in enumerate(temp_poem):
-  #       if poem_line or line in temp_poem:
-  #         continue #don't duplicate a line that's already in the poem
-  #       if self.validate_line(temp_poem, line, line_index):
-  #         if self.debug:
-  #           print("Filled line " + str(line_index) + " with \"" + line + "\"")
-  #         if self.poems[poem_index] != ([None] * self.lines_needed):
-  #           extra_poems.append(list(self.poems[poem_index])) #append in a copy of the same poem, without the new line
-  #         self.poems[poem_index][line_index] = line
-  #         #early return: I don't think this saves much time, so I'm cutting it out for now.
-  #         # if len(   filter( lambda x: len(  filter(lambda y: y == None, x)) == len(x), self.poems)   ) == 0:
-  #         #   self.poems.append([None] * self.lines_needed)
-  #         # if len(extra_poems) > 0:
-  #         #   print "Added: " + str(len(extra_poems))
-  #         #   self.poems.extend(extra_poems)
-
-  #         # #TODO: don't early return just because the line fits well.
-  #         # return False in map(lambda x: None in x, self.poems) # return false until the poem is done. 
-
-  #   #else, try splitting the line.
-  #   #only do this if the number of syllables in the line is equal to this line plus next line's syllable counts.
-
-  #     #if we've gotten here, the line doesn't fit by itself.
-  #       if (line_index < (len(temp_poem)-1)): #if we're not trying to fill the last line of the poem 
-  #         line_syllable_counts = map(lambda x: self.rhyme_checker.count_syllables(x), line.split(" "))
-  #         line_syllable_counts_need_to_be = [0,0]
-  #         if isinstance(self.format["syllable_structure"][line_index], int):
-  #           line_syllable_counts_need_to_be[0] += self.format["syllable_structure"][line_index]
-  #           line_syllable_counts_need_to_be[1] += self.format["syllable_structure"][line_index]
-  #         else:
-  #           line_syllable_counts_need_to_be[0] += self.format["syllable_structure"][line_index][0]
-  #           line_syllable_counts_need_to_be[1] += self.format["syllable_structure"][line_index][1]
-  #         if (line_index < (len(temp_poem)-2)):
-  #           if isinstance(self.format["syllable_structure"][line_index + 1], int):
-  #             line_syllable_counts_need_to_be[0] += self.format["syllable_structure"][line_index + 1]
-  #             line_syllable_counts_need_to_be[1] += self.format["syllable_structure"][line_index + 1]
-  #           else:
-  #             line_syllable_counts_need_to_be[0] += self.format["syllable_structure"][line_index + 1][0]
-  #             line_syllable_counts_need_to_be[1] += self.format["syllable_structure"][line_index + 1][1]
-
-  #         possible_to_split = line_syllable_counts_need_to_be[0] <= sum(line_syllable_counts) and line_syllable_counts_need_to_be[1] >= sum(line_syllable_counts)
-  #         if possible_to_split:
-  #           split_lines = self.split_line_at_syllable_count(line, self.format["syllable_structure"][line_index]) 
-  #           if split_lines:
-  #             for split_line in split_lines:
-  #               temp_temp_poem = list(temp_poem)
-  #               has_failed_yet = False
-  #               for temp_line_index, temp_line in enumerate(split_line):
-  #                 if self.validate_line(temp_temp_poem, temp_line, line_index + temp_line_index):
-  #                   temp_temp_poem[line_index + temp_line_index] = temp_line
-  #                 else:
-  #                   has_failed_yet = True
-  #                   break
-  #               if not has_failed_yet:
-  #                 def assign_to_poem(i_and_line):
-  #                   self.poems[poem_index][line_index + i_and_line[0]] = i_and_line[1]
-  #                 if (self.poems[poem_index] != [None] * self.lines_needed):
-  #                   extra_poems.append(list(self.poems[poem_index])) #append in a copy of the same poem, without the new line
-  #                 map(assign_to_poem , enumerate(split_line))
-  #   #add a new poem if there isn't a blank one in the list of poems.
-  #   if len(   filter( lambda x: len(  filter(lambda y: y == None, x)) == len(x), self.poems)   ) == 0:
-  #     self.poems.append([None] * self.lines_needed)
-  #   if len(extra_poems) > 0:
-  #     if self.debug:
-  #       print "Added: " + str(len(extra_poems))
-  #     self.poems.extend(extra_poems)
-  #   return False in map(lambda x: None in x, self.poems) # return false until the poem is done. 
-
   def validate_line(self, poem, line, index):
     """ Return line if given line fits into the format positioned at the given index, false otherwise"""
     #TODO! now: make this return n lines that go into the poem at index, if they do fit.
@@ -283,11 +222,54 @@ class Poemifier:
   # def has_poem(self):
   #   return True in map(lambda x: None not in x, self.poems)
 
+  def _pair_rhyme_lines(self):
+    """Pair lines that rhyme."""
+    #all the lines in any of the dicts are guaranteed to be of acceptable length.
+    #pairs = {"EH" => {6 => ["a", "b", "c"], (9,11) => ["a", "b", "c"]}, "OI" => ...}
+    pairs_by_rhyme = {}
+    pairs_by_syll_count = {}
+    for rime, rhyme_groups in self.rhyme_dict.items():
+      if len(rhyme_groups) == 1 and len(rhyme_groups.values()[0]) == 1:
+        #print("skipping something while pairing")
+        continue
+      inner_pairs_by_syll_count = {}
+      for syllable_count_token, rhyme_group in rhyme_groups.items():
+
+        #exclude words from the rhyme group whose last word is the last word of another line in this rhyme group
+        already_used_last_words = set()
+        new_rhyme_group = []
+        for rhyme_line in rhyme_group:
+          if rhyme_line.split(" ")[-1] not in already_used_last_words:
+            already_used_last_words.add(rhyme_line.split(" ")[-1])
+            new_rhyme_group.append(rhyme_line)
+
+        inner_pairs_by_syll_count[syllable_count_token] = new_rhyme_group
+        if syllable_count_token not in pairs_by_syll_count.keys():
+          pairs_by_syll_count[syllable_count_token] = {}
+        pairs_by_syll_count[syllable_count_token][rime] = new_rhyme_group
+      pairs_by_rhyme[rime] = inner_pairs_by_syll_count
+    return pairs_by_syll_count
+
   def get_poem(self, random=False):
     """ Return False or a poem. """
     #TODO: again, abstraction!
-    poem = self.lines_needed * [None]
-    if self.format["syllable_structure"]:
+    poem = [None] * self.format["lines_needed"]
+    if self.format["rhyme_scheme"]:
+      print "pairs: " +  str(self._pair_rhyme_lines())
+      pairs = self._pair_rhyme_lines()
+      for syllable_count_token in self.format["unique_syllable_structure"]:
+        if syllable_count_token not in pairs:
+          return False
+        this_sylls_lines = filter(lambda rhymes: len(rhymes) >= self.format["syllable_structure"].count(syllable_count_token), pairs[syllable_count_token].values())
+        if not this_sylls_lines:
+          return False
+        this_sylls_lines = list(this_sylls_lines[0])
+        for index, syllable_count in enumerate(self.format["syllable_structure"]):
+          print(this_sylls_lines)
+          if syllable_count == syllable_count_token:
+            poem[index] = this_sylls_lines.pop()
+      return poem
+    elif self.format["syllable_structure"]:
       #TODO: delete all the hash entries that don't fit anything in the syllable structure
       for index, syllable_count in enumerate(self.format["syllable_structure"]):
         if syllable_count not in self.syllable_count_dict:
@@ -329,34 +311,40 @@ if __name__ == "__main__":
   
   #lines = ["camping is in tents", "my tree table tries", "between those times I slept none"]
   # lines = ["many words in english rhyme with song", "one two three four five six", "a bee see dee word kicks",
-  # "This is a line that is twenty long and here are ten more ending in wrong", "Jeremy Bee Merrill plays ping pong",
-  # ]
+  #  "This is a line that is twenty long", "here are ten more ending in wrong", "Jeremy Bee Merrill plays ping pong",
+  #  ]
 
-  p = Poemifier("haiku")
+  p = Poemifier("limerick")
   p.debug = True
   #TODO: make this a do... while (so we quit when we finish a poem)
   for line in lines:
     line = re.sub("[^A-Za-z ']", "", line)
     line = line.strip()
-    if re.search("\s[BCDEFGHJKLMNOPQRSTUVWXYZ]\s", line):
+    if re.search("\s[BCDEFGHJKLMNOPQRSTUVWXYZ]\s", line): # or re.search("\s[BCDEFGHJKLMNOPQRSTUVWXYZ]$", line) :
       continue
-    if p.try_line(line):
-      if p.debug: 
-        print "Done!"
-      break
-    print p.syllable_count_dict
+    if "No" in line:
+      continue
+    p.add_line(line)
+    # if p.try_line(line):
+    #   if p.debug: 
+    #     print "Done!"
+    #   break
+    # print p.syllable_count_dict
+    # print p.rhyme_dict
+    # print ""
   print ""
   print p.get_poem()
 
 
+#TODO: make split lines work.
+
 #TODO: (eventually)
-# Done? be a little more recursive, but not all the way, about pairing lines. maybe one loop per rhyming group? (i.e. for "a"s)
 # Allow multiple poems to be requested (only break when the number of complete poems in self.poems = the number requested)
 
 """
   #TODO! fix this: 
   lines = ["many words in english rhyme with song", "one two three four five six", "a bee see dee word kicks",
-  "This is a line that is twenty long and here are ten more ending in wrong", "Jeremy Bee Merrill plays ping pong",
+  "This is a line that is twenty long and here are ten more ending in bong", "Jeremy Bee Merrill plays ping pong",
   ]
   the ordering is wrong. if the first line is moved to the end, it works.
   Find a recursive-y way to get the right result here.
